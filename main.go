499ee194cebd17a6387d46ffce72644a66ba4939
Ethan  Hodges <ethan@Ethans-HackBook-Pro.local>
2022-06-06 17:11:50 -0400

    added-signup

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+    "golang.org/x/crypto/bcrypt"
 	//"errors"
 )
 
@@ -81,7 +82,8 @@ func loginEndpoint(c *gin.Context) {
     //stmt, err := DB.Prepare("select password from users where username = '?'");
     rows, err := DB.Query("select password from users where username = $1", input.Username);
     if err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        fmt.Println(gin.H{"error": err.Error()})
+        c.JSON(http.StatusBadRequest, "Authenication Failed")
         return
     }
 
@@ -91,13 +93,50 @@ func loginEndpoint(c *gin.Context) {
         var password string
         err := rows.Scan(&password)
         if err != nil {
-            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+            fmt.Println(gin.H{"error": err.Error()})
+            c.JSON(http.StatusBadRequest, "Authenication Failed")
             return
         }
-        c.JSON(http.StatusOK, password);
-        return
+        if input.Password == password {
+            c.JSON(http.StatusOK, "Logged in")
+            return
+        } else {
+            c.JSON(http.StatusBadRequest, "Authenication Failed")
+            return
+        }
     }
 }
+
+func HashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
+
+type signupInput struct {
+    Name string `json:"name"` 
+    Username string `json:"username"` 
+    Password string `json:"password"` 
+    Email string `json:"email"` 
+}
+
+func signupEndpoint(c *gin.Context) {
+    var input signupInput;
+    err := c.ShouldBindJSON(&input); 
+    if err != nil { fmt.Println(err.Error()); c.JSON(http.StatusBadRequest, "Signup Failed: Incorrect Input"); return }
+    hashedpassword, err := HashPassword(input.Password);
+    if err != nil { fmt.Println(err.Error()); c.JSON(http.StatusBadRequest, "Signup Failed: Password unhashable"); return }
+    _, err = DB.Exec("insert into users (name, username, password, email) values ($1, $2, $3, $4);", input.Name, input.Username, hashedpassword, input.Email);
+    if err != nil { fmt.Println(err.Error()); c.JSON(http.StatusBadRequest, "Signup Failed: Database Rejection"); return }
+    c.JSON(http.StatusOK,"Account Created"); 
+}
+
+
+
 
 func main() {
     intializeDB();
@@ -105,5 +144,7 @@ func main() {
 	router := gin.Default()
     router.GET("/getusers", getDataExample)
     router.POST("/login", loginEndpoint)
+    router.POST("/signup", signupEndpoint)
 	router.Run("localhost:8080")
+
 }
